72d2f67ade1b916431f59d3e01ab2493091ffdd9
Dan Brandt <dan.p.brandt@gmail.com>
2024-01-05 16:55:24 -0600

    modified day.js to match desired format. added js to handle form sumbission.

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,49 @@
 var today = dayjs();
-var reformatDate = today.format('dddd, MMMM D YYYY, h:mm:ss a');
-    $('#3a').text(reformatDate);
+var reformatDate = today.format("dddd, MMMM D YYYY, h:mm:ss a");
+$("#3a").text(reformatDate);
 
 function updateTime() {
-    today = dayjs();
-    reformatDate = today.format('dddd, MMMM D YYYY, h:mm:ss a');
-    $('#3a').text(reformatDate);
+  var today = dayjs();
+  var reformatDate = today.format("MMM D, YYYY at h:mm:ss A"); // Adjusted format
+  $("#3a").text(reformatDate);
 }
 interval = setInterval(() => {
-    updateTime()
+  updateTime();
 }, 1000);
 
-var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
+var projectModal = document.getElementById("projectModal");
+var myInput = document.getElementById("myInput");
 
-myModal.addEventListener('shown.bs.modal', function () {
-    myInput.focus()
-})
+projectModal.addEventListener("shown.bs.modal", function () {
+  myInput.focus();
+});
 
+$('#projectForm').on('submit', function(e) {
+    e.preventDefault(); // prevent default form submission i.e. reload page
+
+    // retrieve form data
+    var projectName = $('#projectName').val();
+    var projectType = $('#projectType').val();
+    var dueDate = $('#dueDate').val();
+
+    // create a new row in the table
+    $('.addRow').append(`
+        <div class="row">
+            <div class="col-4">${projectName}</div>
+            <div class="col-4">${projectType}</div>
+            <div class="col-3">${dueDate}</div>
+            <div class="col-1"><button class="btn btn-danger btn-sm remove">Remove</button></div>
+        </div>
+    `);
+
+    // clear the form fields
+    $('#projectForm')[0].reset();
+
+    // hide the modal
+    $('#projectModal').modal('hide');
+});
+
+// remove a project row
+$(document).on('click', '.remove', function() {
+    $(this).closest('.row').remove();
+});
